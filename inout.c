922214b7c6332d460552b32c6bd79394178e2e70
Simone Tiberi <tib.sim.98@gmail.com>
2020-06-03 14:28:36 +0200

    Riformattato inout.c

--- inout.c
+++ inout.c
@@ -12,101 +12,103 @@ static void handler(int s) { signo = s; }
 
 size_t get_input(unsigned int length, char *string, bool hide, bool not_null)
 {
-	char c;
-	unsigned int i;    
-    	struct sigaction sa, savealrm, saveint, savehup, savequit, saveterm, savetstp, savettin, savettou;
-	struct termios term, oterm;
+    char c;
+    unsigned int i;    
+    struct sigaction sa, savealrm, saveint, savehup, savequit, saveterm, savetstp, savettin, savettou;
+    struct termios term, oterm;
 
-    	if (hide) {
-        	(void) fflush(stdout);
-        	(void) sigemptyset(&sa.sa_mask);
-        	sa.sa_flags = SA_INTERRUPT;
-        	sa.sa_handler = handler;
+    if (hide) {
+        (void) fflush(stdout);
+        (void) sigemptyset(&sa.sa_mask);
+        sa.sa_flags = SA_INTERRUPT;
+        sa.sa_handler = handler;
 
-		(void) sigaction(SIGALRM, &sa, &savealrm);
-		(void) sigaction(SIGINT, &sa, &saveint);
-		(void) sigaction(SIGHUP, &sa, &savehup);
-		(void) sigaction(SIGQUIT, &sa, &savequit);
-		(void) sigaction(SIGTERM, &sa, &saveterm);
-		(void) sigaction(SIGTSTP, &sa, &savetstp);
-		(void) sigaction(SIGTTIN, &sa, &savettin);
-		(void) sigaction(SIGTTOU, &sa, &savettou);
+        (void) sigaction(SIGALRM, &sa, &savealrm);
+        (void) sigaction(SIGINT, &sa, &saveint);
+        (void) sigaction(SIGHUP, &sa, &savehup);
+        (void) sigaction(SIGQUIT, &sa, &savequit);
+        (void) sigaction(SIGTERM, &sa, &saveterm);
+        (void) sigaction(SIGTSTP, &sa, &savetstp);
+        (void) sigaction(SIGTTIN, &sa, &savettin);
+        (void) sigaction(SIGTTOU, &sa, &savettou);
 
-		if (tcgetattr(fileno(stdin), &oterm) == 0) {
-			(void) memcpy(&term, &oterm, sizeof(struct termios));
-			term.c_lflag &= ~(ECHO|ECHONL);
-			(void) tcsetattr(fileno(stdin), TCSAFLUSH, &term);
-		} else {
-			(void) memset(&term, 0, sizeof(struct termios));
-			(void) memset(&oterm, 0, sizeof(struct termios));
-		}
-    	}
+        if (tcgetattr(fileno(stdin), &oterm) == 0) {
+            (void) memcpy(&term, &oterm, sizeof(struct termios));
+            term.c_lflag &= ~(ECHO|ECHONL);
+            (void) tcsetattr(fileno(stdin), TCSAFLUSH, &term);
+            
+        } else {
+            (void) memset(&term, 0, sizeof(struct termios));
+            (void) memset(&oterm, 0, sizeof(struct termios));
+        }
+    }
 
 read_loop:
-	for (i = 0; i < length; ++i) {
-		(void) fread(&c, sizeof(char), 1, stdin);
-		if (c == '\n') {
-			string[i] = '\0';
-			break;
-		} 
-		else
-			string[i] = c;    
-	}
+    for (i = 0; i < length; ++i) {
+        (void) fread(&c, sizeof(char), 1, stdin);
+        if (c == '\n') {
+            string[i] = '\0';
+            break;
+        } else {
+            string[i] = c;  
+        }
+    }
 
-	if (not_null && i == 0)
-		goto read_loop;
-		
-	if (i == length - 1)
-        	string[i] = '\0';
+    if (not_null && i == 0)
+        goto read_loop;
+    
+    if (i == length - 1)
+        string[i] = '\0';
 
-	if (strlen(string) >= length) {	
-        	do {
-            		c = getchar();
-        	} while (c != '\n');
-    	}
+    if (strlen(string) >= length) {	
+        do {
+            c = getchar();
+        } while (c != '\n');
+    }
 
-    	if (hide) {
-		(void) write(fileno(stdout), "\n", 1);
-		(void) tcsetattr(fileno(stdin), TCSAFLUSH, &oterm);
-		(void) sigaction(SIGALRM, &savealrm, NULL);
-		(void) sigaction(SIGINT, &saveint, NULL);
-		(void) sigaction(SIGHUP, &savehup, NULL);
-		(void) sigaction(SIGQUIT, &savequit, NULL);
-		(void) sigaction(SIGTERM, &saveterm, NULL);
-		(void) sigaction(SIGTSTP, &savetstp, NULL);
-		(void) sigaction(SIGTTIN, &savettin, NULL);
-		(void) sigaction(SIGTTOU, &savettou, NULL);
+    if (hide) {
+        (void) write(fileno(stdout), "\n", 1);
+        (void) tcsetattr(fileno(stdin), TCSAFLUSH, &oterm);
+        (void) sigaction(SIGALRM, &savealrm, NULL);
+        (void) sigaction(SIGINT, &saveint, NULL);
+        (void) sigaction(SIGHUP, &savehup, NULL);
+        (void) sigaction(SIGQUIT, &savequit, NULL);
+        (void) sigaction(SIGTERM, &saveterm, NULL);
+        (void) sigaction(SIGTSTP, &savetstp, NULL);
+        (void) sigaction(SIGTTIN, &savettin, NULL);
+        (void) sigaction(SIGTTOU, &savettou, NULL);
 
-		if (signo)
-			(void) raise(signo);
-	}
-	
-	return i;
+        if (signo)
+            (void) raise(signo);
+    }
+
+    return i;
 }
 
 char multi_choice(const char *question, const char *choices, int no_choices)
 {
-	char choices_str[2 * no_choices * sizeof(char)];
-	int i, j = 0;
+    char choices_str[2 * no_choices * sizeof(char)];
+    int i, j = 0;
 
-	for (i = 0; i < no_choices; ++i) {
-		choices_str[j++] = choices[i];
-		choices_str[j++] = '/';
-	}
-	choices_str[j-1] = '\0';
+    for (i = 0; i < no_choices; ++i) {
+        choices_str[j++] = choices[i];
+        choices_str[j++] = '/';
+    }
+    
+    choices_str[j-1] = '\0';
 
-	while (true) {
-		printf("%s [%s]: ", question, choices_str);
+    while (true) {
+        printf("%s [%s]: ", question, choices_str);
 
-		char c;
-		get_input(1, &c, false, true);
-		c = tolower(c);
+        char c;
+        get_input(1, &c, false, true);
+        c = tolower(c);
 
-		for (i = 0; i < no_choices; ++i) {
-			if(c == tolower(choices[i]))
-				return c;
-		}
+        for (i = 0; i < no_choices; ++i) {
+            if(c == tolower(choices[i]))
+                return c;
+        }
 
-		printf("Sorry not compliant input, please retry!\n");
-	}
+        printf("Sorry not compliant input, please retry!\n");
+    }
 }
